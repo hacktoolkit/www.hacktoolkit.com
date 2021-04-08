7e851618f579ac2052a83db9c680be26379db734
yashenafi@cloudxdpo.com <yashenafi@cloudxdpo.com>
2021-04-08 12:53:06 +0300

    Javascript issue fixed on Link

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Page from '../components/base_page';
-import '../components/js/text_effect';
-
+import Welcome from '../components/js/text_effect';
 import css from '../styles/common.module.scss';
-
+import homecss from '../styles/Home.module.scss';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -10,14 +9,20 @@ import Col from 'react-bootstrap/Col';
 export default function Home() {
     return (
         <Page>
-            <div className={css.title}>
-                <div className={css.title_max}>
-                    Open-source <span className="text"></span>
-                </div>
-                <div className={css.mini_title}>
-                Win hackathons by using refined APIs and bootstrap code to build complete websites and apps in 24 hours. 
+            <div className={homecss.white_section}>
+                <div className={css.title}>
+                    <div className={css.title_max}>
+                        Open-source <Welcome /> <span className="text"></span>
+                    </div>
+                    <div className={css.mini_title}>
+                    Win hackathons by using refined APIs and bootstrap code to build complete websites and apps in 24 hours. 
+                    </div>
                 </div>
             </div>
+            <div className={homecss.black_section}>
+                <h2>Second Section</h2>
+            </div>
+
         </Page>
     );
 }
