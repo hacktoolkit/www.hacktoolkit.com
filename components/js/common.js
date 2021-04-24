2ba81aba821794837fb745f3cd9c438dec02312b
yashenafi@cloudxdpo.com <yashenafi@cloudxdpo.com>
2021-04-24 21:12:51 +0300

    tracking number update

--- components/js/common.js
+++ components/js/common.js
@@ -1,6 +1,6 @@
 import { GoogleAnalytics } from './google/analytics';
 
 export function CommonJS() {
-    const jsx = <GoogleAnalytics trackingId="G-6HNBH1SW7K" />;
+    const jsx = <GoogleAnalytics trackingId="UA-43807579-1" />;
     return jsx;
 }
